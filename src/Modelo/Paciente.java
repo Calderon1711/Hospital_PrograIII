77284712213bb0b1cc8a383ddf0135edce2843eb
AleTrao <alejandraoba54@gmail.com>
2025-08-31 13:39:03 -0600

    clase Paciente

---
+++ src/Modelo/Paciente.java
@@ -0,0 +1,59 @@
+package Modelo;
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Paciente {
+    private String id;
+    private String nombre;
+    private LocalDate fechaNacimiento;
+    private int telefono;
+
+    public Paciente(String id, String nombre, LocalDate fechaNacimiento, int telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.telefono = telefono;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fecha) {
+        this.fechaNacimiento = fecha;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente{" +
+                "id='" + getId() + '\'' +
+                ", nombre='" + getNombre() + '\'' +
+                ", fecha=" + getFechaNacimiento() +
+                ", telefono=" + getTelefono() +
+                '}';
+    }
+}
