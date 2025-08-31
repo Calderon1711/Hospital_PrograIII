0eefe8ee8b9a72497cd94a09721799b038ecc756
LHidalgo23 <luis.hidalgo.calvo@est.una.ac.cr>
2025-08-31 13:19:18 -0600

    Package Modelo

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        Personal p1 = new Personal("305470210","34562","lUIS");
-        System.out.println(p1.toString());
-
-
-    }
-}
