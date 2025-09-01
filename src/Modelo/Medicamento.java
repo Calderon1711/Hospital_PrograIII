43c82217ca402ec34cad6915a9c5bd438900c44d
Calderon1711 <alejandro.calderon.campos@est.una.ac.cr>
2025-09-01 15:11:00 -0600

    Medicamento Listo

---
+++ src/Modelo/Medicamento.java
@@ -0,0 +1,46 @@
+package Modelo;
+
+public class Medicamento {
+   private String codigo;
+   private String nombre;
+   private String presentacion;
+
+   public Medicamento(String codigo, String nombre, String presentacion) {
+       this.codigo = codigo;
+       this.nombre = nombre;
+       this.presentacion = presentacion;
+   }
+
+   public Medicamento(){}
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getPresentacion() {
+        return presentacion;
+    }
+
+    public void setPresentacion(String presentacion) {
+        this.presentacion = presentacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+       this.nombre = nombre;
+    }
+    @Override
+    public String toString() {
+       return "Medicamento{" +
+               "nombre='" + nombre + '\'' +
+               ", codigo='" + codigo + '\'' +
+               ", presentacion='" + presentacion + '\'' +
+               '}';
+    }
+}
