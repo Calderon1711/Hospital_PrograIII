a8db6df1d7c658f514698b148908c372a27f8207
Calderon1711 <alejandro.calderon.campos@est.una.ac.cr>
2025-09-03 13:32:04 -0600

    controlador

--- src/Vista/LoginVista.java
+++ src/Vista/LoginVista.java
@@ -1,4 +1,11 @@
 package Vista;
 
+import javax.swing.*;
+
 public class LoginVista {
+    private JPanel panel1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
