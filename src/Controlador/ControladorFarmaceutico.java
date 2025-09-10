0bab53c477d4a1facde8e9c4fa15ec4413d486f2
AleTrao <alejandraoba54@gmail.com>
2025-09-10 11:34:59 -0600

    farmaceuta

---
+++ src/Controlador/ControladorFarmaceutico.java
@@ -0,0 +1,9 @@
+package Controlador;
+
+import Modelo.*;
+import Vista.FarmaceuticoVista;
+
+import javax.swing.*;
+public class ControladorFarmaceutico {
+ //   private final
+}
---
+++ src/Modelo/EstadoReceta.java
@@ -0,0 +1,8 @@
+package Modelo;
+
+public enum EstadoReceta {
+    CONFECCIONADA,
+    PROCESO,
+    LISTA,
+    ENTREGADA
+}
